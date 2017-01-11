bb5c7e9b08746c51702031f531e167f412a855ea
Daniel Jackoway <commits@danieljackoway.com>
2017-01-11 00:15:11 -0800

    mailchimp wip

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from mailchimp3 import MailChimp
 
 def create_app():
   app = Flask(__name__)
@@ -7,8 +10,26 @@ def create_app():
 
   return app
 
+def mailchimp_client():
+    user = os.environ.get('MAILCHIMP_USER')
+    secret = os.environ.get('MAILCHIMP_SECRET')
+    enabled = os.environ.get('MAILCHIMP_ENABLE', 'false').lower()[0] == 't'
+    return MailChimp(user, secret, enabled)
+
+MAILCHIMP_LIST = os.environ.get('MAILCHIMP_LIST')
+MAILCHIMP = mailchimp_client()
+
 app = create_app()
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    email = request.form['email']
+    MAILCHIMP.lists.members.create(MAILCHIMP_LIST, {
+        'email_address': email,
+        'status': 'subscribed'
+    })
+    return "ok " + email
